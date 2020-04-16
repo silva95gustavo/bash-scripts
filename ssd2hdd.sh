e69b1d42dd8232883a920074ab4072dc00729b0f
Gustavo Silva <silva95gustavo@gmail.com>
2020-04-16 14:12:00 +0100

    Add ssd2hdd.sh

---
+++ ssd2hdd.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ -z "$1" ]
+  then
+    echo "Usage: $0 path"
+    exit 1
+fi
+
+HDD_PATH_PREFIX="/media/hdd" # Set to your HDD mount point
+
+real_path=$(realpath $1)
+hdd_path="${HDD_PATH_PREFIX}${real_path}"
+
+mkdir -p ${hdd_path}
+
+# TODO: Perform operation atomically instead of in two steps
+mv "${real_path}" "${hdd_path}"
+ln -s "${hdd_path}" "${real_path}"
