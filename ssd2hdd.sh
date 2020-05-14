3a6a2ab2fe46b339e26baef3ae43811a789b85b7
Gustavo Silva <silva95gustavo@gmail.com>
2020-05-14 19:03:24 +0100

    Fix ssd2hdd.sh script

--- ssd2hdd.sh
+++ ssd2hdd.sh
@@ -8,11 +8,12 @@ fi
 
 HDD_PATH_PREFIX="/media/hdd" # Set to your HDD mount point
 
-real_path="$(realpath $1)"
+real_path=$(realpath "$1")
 hdd_path="${HDD_PATH_PREFIX}${real_path}"
+hdd_dir=$(dirname "${hdd_path}")/
 
-mkdir -p ${hdd_path}
+mkdir -p "${hdd_path}"
 
 # TODO: Perform operation atomically instead of in two steps
-mv "${real_path}" "${hdd_path}"
+mv "${real_path}" "${hdd_dir}"
 ln -s "${hdd_path}" "${real_path}"
